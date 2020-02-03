a976c077680647e318d245f85fee7ca835b32485
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-02-03 11:48:56 -0500

    fixing things

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,33 +1,33 @@
 
 class Scraper
-    attr_accessor :body_type, :info, :ecto_info, :doc
+
 
 
     @@doc = Nokogiri::HTML(open("https://www.bodybuilding.com/fun/becker3.htm"))
     
 
     def ecto_info
-    ectomorph_info = puts "#{@@doc.search("#DPG p").children[4..8].text}"
+    ectomorph_info = @@doc.search("#DPG p").children[4..8].text
     end
     
     def ecto_training
-    ectomorph_training = puts "#{@@doc.search("#DPG p").children[18..20].text}"
+    ectomorph_training = puts @@doc.search("#DPG p").children[18..20].text
     end
   
     def meso_info
-    mesomorph_info = puts "#{@@doc.search("#DPG p").children[9..10].text}"
+    mesomorph_info = puts @@doc.search("#DPG p").children[9..10].text
     end
 
     def meso_training
-    mesomorph_training = puts "#{@@doc.search("#DPG p").children[22..24].text}"
+    mesomorph_training = puts @@doc.search("#DPG p").children[22..24].text
     end
 
     def endo_info
-    endomorph_info = puts "#{@@doc.search("#DPG p").children[11..12].text}"
+    endomorph_info = puts @@doc.search("#DPG p").children[11..12].text
     end
 
     def endo_training
-    endomorph_train = puts "#{@@doc.search("#DPG p").children[26..28].text}"
+    endomorph_train = puts @@doc.search("#DPG p").children[26..28].text
     end
     
     def in_info
